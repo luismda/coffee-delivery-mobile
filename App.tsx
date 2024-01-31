e0006582ab4183087fa42e195f8a93bca661deb0
Luís Miguel <luismigueldutraalves@gmail.com>
2024-01-30 22:01:02 -0300

    chore: add google fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
+import { useCallback, useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import * as SplashScreen from 'expo-splash-screen'
+import { useFonts, Baloo2_700Bold } from '@expo-google-fonts/baloo-2'
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Baloo2_700Bold,
+    Roboto_700Bold,
+    Roboto_400Regular,
+  })
+
+  const hideSplashScreen = useCallback(async () => {
+    if (hasLoadedFonts) {
+      await SplashScreen.hideAsync()
+    }
+  }, [hasLoadedFonts])
+
+  useEffect(() => {
+    hideSplashScreen()
+  }, [hideSplashScreen])
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
